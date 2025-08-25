3bc93cd835f78d4bafecda1eb949c9681e799054
Ivan Kosov <kosov.vanya@gmail.com>
2025-08-24 20:56:45 -0500

    Added support for GitHub repositories
    Closes #47

--- internal/launcher/launcher.go
+++ internal/launcher/launcher.go
@@ -2,11 +2,17 @@ package launcher
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"iter"
 	"log/slog"
 	"path"
+	"strings"
 
 	"github.com/AntonKosov/git-backups/internal/clog"
 	"github.com/AntonKosov/git-backups/internal/config"
+	"github.com/AntonKosov/git-backups/internal/github"
+	"github.com/AntonKosov/git-backups/internal/slice"
 )
 
 //counterfeiter:generate . BackupService
@@ -14,7 +20,12 @@ type BackupService interface {
 	Run(ctx context.Context, url, targetFolder string) error
 }
 
-func Run(ctx context.Context, conf config.Config, backupService BackupService) error {
+//counterfeiter:generate . ReaderService
+type ReaderService interface {
+	AllRepos(ctx context.Context, token, affiliation string) iter.Seq2[github.Repo, error]
+}
+
+func Run(ctx context.Context, conf config.Config, backupService BackupService, readerService ReaderService) error {
 	slog.InfoContext(ctx, "Beginning to backup generic repositories...")
 	err := backupGeneric(ctx, conf.Repositories.Generic, backupService)
 	if err != nil {
@@ -23,7 +34,7 @@ func Run(ctx context.Context, conf config.Config, backupService BackupService) e
 	slog.InfoContext(ctx, "Backed up generic repositories")
 
 	slog.InfoContext(ctx, "Beginning to backup github repositories...")
-	err = backupGitHub(ctx, conf.Repositories.GitHub, backupService)
+	err = backupGitHub(ctx, conf.Repositories.GitHub, backupService, readerService)
 	if err != nil {
 		return err
 	}
@@ -48,7 +59,91 @@ func backupGeneric(ctx context.Context, genericConf []config.GenericRepo, backup
 	return nil
 }
 
-func backupGitHub(context.Context, []config.GitHubRepo, BackupService) error {
-	// TODO
+func backupGitHub(ctx context.Context, githubConf []config.GitHubRepo, backupService BackupService, readerService ReaderService) error {
+	for _, profile := range githubConf {
+		ctx := clog.Add(ctx, "profile", profile)
+		repos := include(
+			profile.Include,
+			exclude(
+				profile.Exclude,
+				readerService.AllRepos(ctx, profile.Token, profile.Affiliation),
+			),
+		)
+		for repo, err := range repos {
+			if err != nil {
+				slog.ErrorContext(ctx, "Failed to read repositories", "error", err)
+				return err
+			}
+
+			ctx := clog.Add(ctx, "repo", repo.Name)
+
+			urlWithToken, err := addTokenToGithubURL(repo.CloneURL, profile.Token)
+			if err != nil {
+				slog.ErrorContext(ctx, "Failed to add token to URL", "error", err)
+				return err
+			}
+
+			if err := backupService.Run(ctx, urlWithToken, path.Join(profile.RootFolder, repo.Owner, repo.Name)); err != nil {
+				slog.ErrorContext(ctx, "Failed to backup", "error", err)
+				return err
+			}
+		}
+	}
 	return nil
+}
+
+func include(toInclude []string, repos iter.Seq2[github.Repo, error]) iter.Seq2[github.Repo, error] {
+	if toInclude == nil {
+		return repos
+	}
+
+	includeMap := slice.Lookup(toInclude, func(name string) (string, bool) { return strings.ToLower(name), true })
+	return func(yield func(github.Repo, error) bool) {
+		for repo, err := range repos {
+			if err != nil {
+				yield(github.Repo{}, err)
+				return
+			}
+
+			if !includeMap[strings.ToLower(repo.Name)] {
+				continue
+			}
+
+			if !yield(repo, nil) {
+				return
+			}
+		}
+	}
+}
+
+func exclude(toExclude []string, repos iter.Seq2[github.Repo, error]) iter.Seq2[github.Repo, error] {
+	if len(toExclude) == 0 {
+		return repos
+	}
+
+	excludeMap := slice.Lookup(toExclude, func(name string) (string, bool) { return strings.ToLower(name), true })
+	return func(yield func(github.Repo, error) bool) {
+		for repo, err := range repos {
+			if err != nil {
+				yield(github.Repo{}, err)
+				return
+			}
+
+			if excludeMap[strings.ToLower(repo.Name)] {
+				continue
+			}
+
+			if !yield(repo, nil) {
+				return
+			}
+		}
+	}
+}
+
+func addTokenToGithubURL(url, token string) (string, error) {
+	if !strings.HasPrefix(url, "https://") {
+		return "", errors.New("unexpected URL prefix")
+	}
+
+	return fmt.Sprintf("https://oauth2:%v@%v", token, url[8:]), nil
 }
